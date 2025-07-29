6db7b273ec4edb135a27ecdc5c84d0471a8f5648
Danial Farooq <danialf64@gmail.com>
2025-07-29 14:42:29 +0100

    Add different versions of demand modeling

--- analysis/required_global_fleet.py
+++ analysis/required_global_fleet.py
@@ -1,17 +1,17 @@
 """Analysis to determine the required size of the global fleet."""
 
 import camia_engine as engine
-from camia_model.units import day, year
+from camia_model.units import day
 
 import aviation
 from aviation.units import aircraft, journey, passenger
 
-passengers_per_year = 5_000_000_000.0 * passenger / year
 seats_per_aircraft = 160.0 * passenger / aircraft
 flights_per_aircraft_per_day = 3.6 * journey / (aircraft * day)
-
+modeling_year = list(range(2025, 2051))
 inputs = {
-    "passengers_per_year": passengers_per_year,
+    "demand_change_model": aviation.DemandChangeModel.EXPONENTIAL,
+    "modeling_year": modeling_year,
     "seats_per_aircraft": seats_per_aircraft,
     "flights_per_aircraft_per_day": flights_per_aircraft_per_day,
 }
--- src/aviation/__init__.py
+++ src/aviation/__init__.py
@@ -5,8 +5,20 @@ Modules:
         data.
 """
 
-__all__ = ("passengers_per_day", "required_global_fleet")
+__all__ = ("DemandChangeModel", "transforms")
 
+from aviation.demand import (
+    DemandChangeModel,
+    demand_change_model,
+    demand_change_rate,
+    passengers_per_year,
+)
 from aviation.fleet import passengers_per_day, required_global_fleet
 
-transforms = (passengers_per_day, required_global_fleet)
+transforms = (
+    demand_change_model,
+    demand_change_rate,
+    passengers_per_day,
+    passengers_per_year,
+    required_global_fleet,
+)
